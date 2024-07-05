8e56d6253166eedf00c5c7df4475d861450afdb1
VaIice <rlatmdgus499@gmail.com>
2024-07-06 00:31:59 +0900

    phone add

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,11 +1,13 @@
 function AboutMe() {
     return (
     <section>
-        <h2>About Me<span className="highlight">.</span></h2>
-            <h4>Email<span className="highlight">.</span></h4>
-             <span>rlatmdgus499@gmail.com</span>
+            <h2>About Me<span className="highlight">.</span></h2>
             <h4>Education<span className="highlight">.</span></h4>
-             <span>세종대학교 컴퓨터공학과</span>
+            <span>세종대학교 컴퓨터공학과</span>            
+            <h4>Email<span className="highlight">.</span></h4>
+             <span>rlatmdgus499@gmail.com</span>            
+            <h4>Phone<span className="highlight">.</span></h4>
+             <span>010-9423-8723</span>        
     </section>
        )
 }
