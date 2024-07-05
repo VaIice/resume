68a461d792f9dc7d219fc1eff5f27a254604a20e
VaIice <rlatmdgus499@gmail.com>
2024-07-06 00:40:43 +0900

    birth add

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -2,6 +2,8 @@ function AboutMe() {
     return (
     <section>
             <h2>About Me<span className="highlight">.</span></h2>
+            <h4>Birth<span className="highlight">.</span></h4>
+            <span>1999년 11월 6일</span>
             <h4>Education<span className="highlight">.</span></h4>
             <span>세종대학교 컴퓨터공학과</span>            
             <h4>Email<span className="highlight">.</span></h4>
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -5,7 +5,7 @@ function Skills() {
        <h3>Frontend<span className="highlight">.</span></h3>
        <span>HTML, CSS, JavaScript, React, Redux (Redux-toolkit), TypeScript</span>   
        <h3>DevOps<span className="highlight">.</span></h3>
-       <span>Git, Figma, AWS (S3, Cloudfront), Google Analytics</span>   
+       <span>Git, Figma, AWS (S3, Cloudfront)</span>   
   </section>
  )
 }
